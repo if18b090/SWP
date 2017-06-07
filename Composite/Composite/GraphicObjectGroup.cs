2417788d1c41d7f0f6782166b3bacb07c7fb07f8
Verena <if15b028@technikum-wien.at>
2017-06-07 19:13:47 +0200

    Composite

---
+++ Composite/Composite/GraphicObjectGroup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+	public class GraphicObjectGroup : GraphicObject
+	{
+		private List<GraphicObject> objectList = new List<GraphicObject>();
+
+		override public void draw()
+		{
+			foreach (GraphicObject member in objectList)
+			{
+				member.draw();
+			}
+		}
+
+		override public void add(GraphicObject g)
+		{
+			objectList.Add(g);
+		}
+
+		override public void remove(GraphicObject g)
+		{
+			objectList.Remove(g);
+		}
+
+		override public void move(int dx, int dy)
+		{
+			foreach (GraphicObject member in objectList)
+			{
+				member.move(dx, dy);
+			}
+		}
+	}
+}
---
+++ Composite/Composite/Triangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+	public class Triangle : GraphicObject
+	{
+		private Point p1;
+		private Point p2;
+		private Point p3;
+
+		public Triangle(Point p1, Point p2, Point p3)
+		{
+			this.p1 = p1;
+			this.p2 = p2;
+			this.p3 = p3;
+		}
+
+		override public void draw()
+		{
+			Console.WriteLine("TRIANGLE drawn: p1 = " + p1 + ", p2 = " + p2 + " and p3 = " + p3 + ".");
+		}
+
+		override public void add(GraphicObject g)
+		{
+		}
+
+		override public void remove(GraphicObject g)
+		{
+		}
+
+		override public void move(int dx, int dy)
+		{
+			p1.Offset(dx, dy);
+			p2.Offset(dx, dy);
+			p3.Offset(dx, dy);
+			Console.WriteLine("Corners of TRIANGLE now at: p1 = " + p1 + ", p2 = " + p2 + " p3= " + p3 + ".");
+		}
+	}
+}
